7285549ed30bf2038ed1610f236150436f570a65
Baselhammam <147231455+Baselhammam@users.noreply.github.com>
2024-05-13 14:31:16 +0000

    30 - how to manually update data in MongoDB

--- server.js
+++ server.js
@@ -25,3 +25,4 @@ app.use(errorResponserHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// no change in github video 30
